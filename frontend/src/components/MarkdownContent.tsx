12bb2ef87cf69e2e4e7e91f52ea4948f384d5b6a
mocococococo <tomomichi.kanaeda@paiza.co.jp>
2026-08-26 14:15:48 +0900

    チャット表示形式修正

--- frontend/src/components/MarkdownContent.tsx
+++ frontend/src/components/MarkdownContent.tsx
@@ -5,6 +5,24 @@ type MarkdownContentProps = {
   content: string;
 };
 
+export function normalizeMarkdownContent(content: string) {
+  const escapedLineBreaks = content.match(/\\r\\n|\\n|\\r/g)?.length ?? 0;
+  const actualLineBreaks = content.match(/\r\n|\n|\r/g)?.length ?? 0;
+  const normalizedLineBreaks =
+    escapedLineBreaks > actualLineBreaks
+      ? content.replace(/\\r\\n|\\n|\\r/g, '\n')
+      : content;
+
+  return normalizedLineBreaks.replace(
+    /^(\s*(?:[-+*]|\d+[.)]))\u00a0+/gm,
+    '$1 ',
+  );
+}
+
 export default function MarkdownContent({ content }: MarkdownContentProps) {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
+  return (
+    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+      {normalizeMarkdownContent(content)}
+    </ReactMarkdown>
+  );
 }
